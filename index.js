cc67a59ea2ddee48146275cadf044897174aca2e
mariofukuoka <mario-fukuoka@outlook.com>
2023-03-18 01:46:13 +0100

    added GainExperience event id to desc map

--- index.js
+++ index.js
@@ -19,9 +19,19 @@ var trackedPlayers = ['kurohagane', 'lukas1233', 'yippys'];
 
 // ============================= Functions ==============================
 
+async function getGainExperienceIdDescMap() {
+  const url = 'https://census.daybreakgames.com/get/ps2/experience?c:limit=10000'
+  const response = await axios.get(url);
+  const map = response.data.experience_list.reduce( (acc, currEvent) => {
+    acc[currEvent.experience_id] = currEvent.description;
+    return acc;
+  }, {});
+  return map;
+}
+
 function getMemberNames(outfitTag) {
   let memberNames = [];
-  url = `https://census.daybreakgames.com/s:${serviceId}/get/ps2:v2/outfit/?alias_lower=${outfitTag.toLowerCase()}&c:resolve=member_character(name,members_character_id)`
+  const url = `https://census.daybreakgames.com/s:${serviceId}/get/ps2:v2/outfit/?alias_lower=${outfitTag.toLowerCase()}&c:resolve=member_character(name,members_character_id)`
   const res = request('GET', url);
   const data = JSON.parse(res.getBody('utf8'));
   for(member of data.outfit_list[0].members) {
@@ -65,11 +75,14 @@ async function main() {
 
 
   //idNameMap = await getPlayerIdNameMap(trackedPlayers);
-  idNameMap = await getPlayerIdNameMap(getMemberNames('vcbc'));
+  const idNameMap = await getPlayerIdNameMap(getMemberNames('vcbc'));
 
-  trackedIds = Object.keys(idNameMap);
+  const trackedIds = Object.keys(idNameMap);
   console.log(idNameMap);
   //console.log(trackedIds);
+
+  const gainXpIdDescMap = await getGainExperienceIdDescMap();
+  //console.log(gainXpIdDescMap);
 
   // Create a new WebSocket connection to the Daybreak API websocket endpoint
   const ws = new WebSocket(`wss://push.planetside2.com/streaming?environment=ps2&service-id=s:${serviceId}`);
@@ -140,8 +153,12 @@ async function main() {
         hour12: false 
       });
 
-      if (data.payload.event_name)
-      msg = `${formattedDate}: ${data.payload.event_name} event for ${idNameMap[characterId]}`
+      let eventName = data.payload.event_name;
+      if (eventName === 'GainExperience') {
+        eventName = gainXpIdDescMap[data.payload.experience_id];
+      }
+
+      msg = `${formattedDate}: ${eventName} event for ${idNameMap[characterId]}`
       console.log(msg);
       
       // Send a message to a Discord channel
@@ -157,62 +174,3 @@ async function main() {
 }
 
 main();
-
-/*
-// get list of outfit member ids 
-function getMemberIds(outfitTag) {
-  let memberIds = [];
-  url = `https://census.daybreakgames.com/s:${serviceId}/get/ps2:v2/outfit/?alias_lower=${outfitTag.toLowerCase()}&c:resolve=member_character(name,members_character_id)`
-  const res = request('GET', url);
-  const data = JSON.parse(res.getBody('utf8'));
-  for(member of data.outfit_list[0].members) {
-    memberIds.push(member.character_id);
-  }
-  return memberIds;
-}
-
-memberIds = getMemberIds('axig');
-console.log(memberIds[0]);
-
-// subscribe to outfit member events
-subJSON = {
-  service: 'event',
-  action: 'subscribe',
-  characters: memberIds,
-  worlds: ['all'],
-  eventNames: ['Death'],
-  logicalAndCharactersWithWorlds:true
-}
-
-
-// Listen for the 'open' event, indicating that the connection to the websocket has been established
-ws.on('open', () => {
-  console.log('Connected to Daybreak API websocket');
-  
-  // Subscribe to the player login event stream
-  ws.send(JSON.stringify(subJSON));
-});
-
-// Listen for incoming messages from the websocket
-ws.on('message', message => {
-  const data = JSON.parse(message);
-  
-  // Check if the message is a player login event
-  if (data.type === 'serviceMessage') { // && data.payload.event_name === 'PlayerLogin') {
-    const characterId = data.payload.character_id;
-    console.log(memberIds.includes(characterId));
-    const timestamp = data.payload.timestamp;
-    const date = new Date(timestamp * 1000)
-    const formattedDate = date.toLocaleString('en-GB', { 
-      year: 'numeric', 
-      month: '2-digit', 
-      day: '2-digit', 
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: false 
-    });
-    console.log(`Player ${characterId} logged in at ${formattedDate}`);
-  }
-});
-
-*/
